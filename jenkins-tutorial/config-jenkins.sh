c68cced3b8706b2535dd6258b4e6aa147ec8e242
yapingli2022 <cathylllll15@gmail.com>
2023-03-13 14:34:57 -0400

    'changes'

--- jenkins-tutorial/config-jenkins.sh
+++ jenkins-tutorial/config-jenkins.sh
@@ -5,11 +5,11 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo add-apt-repository ppa:webupd8team/java -y
 sudo apt-get update
-echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+echo "oracle-java8-installer and shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
 sudo apt-get install oracle-java8-installer -y
 sudo apt-get install jenkins -y
 
-# Docker
+# Docker1
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo apt-key fingerprint 0EBFCD88
